1944608a9f2193c6572bb1286575601d34831f0b
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-12-25 15:54:49 +0300

    commit

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,80 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public void myFunction1()
+
+    {
+        int number = 5;
+        float decimalNumber = 3.14f;
+        string message = "Hello, Unity!";
+        bool isActive = true;
+
+        void TestFunction()
+        {
+            Debug.Log("Integer: " + number);
+            Debug.Log("Float: " + decimalNumber);
+            Debug.Log("String: " + message);
+            Debug.Log("Boolean: " + isActive);
+        }
+        int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    public void myFunction2()
+    {
+        void Awake() // Awake Fonksiyonu, oyun nesnesi etkinleştirildiğinde çağrılır.
+        {
+            
+        }
+        void Start() // Start Fonksiyonu, oyun nesnesi ilk kez etkinleştirildiğinde çağrılır.
+        {
+            
+        }
+        void Update() // Update Fonksiyonu, her karede bir kez çağrılır.
+        {
+            
+        }
+        void FixedUpdate() // FixedUpdate Fonksiyonu, fizik hesaplamaları için sabit zaman aralıklarında çağrılır.
+        {
+            
+        }
+        void LateUpdate() // LateUpdate Fonksiyonu, tüm Update fonksiyonları çalıştıktan sonra çağrılır.
+        {
+            
+        }
+    }
+    public void myFunction3()
+    {
+        Debug.Log("This is a debug message."); // Konsola bilgi mesajı yazdırır.
+        Debug.LogWarning("This is a warning message."); // Konsola uyarı mesajı yazdırır.
+        Debug.LogError("This is an error message."); // Konsola hata mesajı yazdırır.
+        Debug.LogFormat("Formatted message: {0}, {1}", "Hello", 123); // Formatlı mesaj yazdırır.
+        Debug.Assert(1 + 1 == 2, "Math is broken!"); // Koşul yanlışsa hata mesajı gösterir.
+        // Debug.ClearDeveloperConsole(); // Konsolu temizler.
+    }
+    public void myFunction4()
+    {
+        int S1 = 10;
+        int S2 = 20;
+        if(S1 % 2 == 0 && S2 % 2 == 0)
+        {
+            Debug.Log($"{S1} ve {S2} çift sayılardır.");
+        }
+        else
+        {
+            Debug.Log($"{S1} ve {S2} çift sayılar değildir.");
+        }
+        switch(S1 % 2 == 0 && S2 % 2 == 0)
+        {
+            case true:
+                Debug.Log($"{S1} ve {S2} çift sayılardır.");
+                break;
+            case false:
+                Debug.Log($"{S1} ve {S2} çift sayılar değildir.");
+                break;
+        }
+    }
+}
